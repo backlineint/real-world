3f496b873c84aa411d603963fe779680f1420286
Brian Perry <brian@brianperryinteractive.com>
2017-01-28 16:09:24 -0600

    Added install script

---
+++ scripts/drupal_install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd ../drupal
+drush si -y
+drush upwd admin --password=admin
+drush sql-query "TRUNCATE shortcut"
+drush sql-query "TRUNCATE shortcut_field_data"
+drush cset system.site uuid a63de821-d027-4e5b-aeaa-536392c1221f -y
+drush cim sync -y
